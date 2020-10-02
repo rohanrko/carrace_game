50471dfec1636249c27b38fc5de2c377aff5986d
rohanrko <rohanrko96.6@gmail.com>
2020-10-02 23:08:41 +0530

    Add files via upload

---
+++ Hacktober2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Hacktober2 {
+	public static void main (String[] args) {
+		
+		int r=10;
+		String ans;
+		if (r%2==0) {
+		    ans="even";
+		}
+		else {
+		    ans="odd";
+		}
+		System.out.println(ans);
+		
+	}
+}
